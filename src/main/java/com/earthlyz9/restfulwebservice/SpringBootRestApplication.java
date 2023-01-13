3cc565061a5ee390f4b954de19db1f0caa4dc3d5
linda2927 <linda2927@naver.com>
2023-01-14 08:13:53 +0900

    feat: internationalization of response messages

--- src/main/java/com/earthlyz9/restfulwebservice/SpringBootRestApplication.java
+++ src/main/java/com/earthlyz9/restfulwebservice/SpringBootRestApplication.java
@@ -2,6 +2,11 @@ package com.earthlyz9.restfulwebservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class SpringBootRestApplication {
@@ -10,4 +15,10 @@ public class SpringBootRestApplication {
         SpringApplication.run(SpringBootRestApplication.class, args);
     }
 
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.KOREA);
+        return localeResolver;
+    }
 }
