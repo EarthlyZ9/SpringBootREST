9e127c27b496631100cc20416c2f279451d2b127
linda2927 <linda2927@naver.com>
2023-01-13 22:56:47 +0900

    feat: implement controller with path variable

--- src/main/java/com/earthlyz9/restfulwebservice/HelloWorldController.java
+++ src/main/java/com/earthlyz9/restfulwebservice/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.earthlyz9.restfulwebservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,11 +14,15 @@ public class HelloWorldController {
     }
 
     // GET
-    // @RequestMapping(method=RequestMethod.GET, path="/")
+    // 단순히 String 형태가 아닌 Bean 으로 반환할 경우 json 으로 반환할 수 있음
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
     }
+
+    @GetMapping(path="/hello-world-bean/{name}")
+    public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
 }
 
-// 단순히 String 형태가 아닌 Bean 으로 반환할 경우 json 으로 반환할 수 있음
