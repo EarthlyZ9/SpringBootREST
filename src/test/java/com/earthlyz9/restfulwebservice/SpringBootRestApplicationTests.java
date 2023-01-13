d68b2fdbf72e9f1d6e19221a5eba3fbd538f2534
linda2927 <linda2927@naver.com>
2023-01-13 16:18:52 +0900

    Initial commit

---
+++ src/test/java/com/earthlyz9/restfulwebservice/SpringBootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.earthlyz9.restfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
